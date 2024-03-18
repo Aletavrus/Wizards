e4011004da81adf8ad01041fc5c747ef98ccef08
Jeany <il.karachev@gmail.com>
2024-03-18 21:58:20 +0300

    Reverted changes

--- Wizards/Spells/SpellNotTargeted.cs
+++ Wizards/Spells/SpellNotTargeted.cs
@@ -3,7 +3,7 @@
 abstract class SpellNotTargeted : SpellBase
 {
     protected PlayerBase caster;
-    protected int[] position2;
+    protected int[] position;
     
     public abstract void Cast(int castX, int castY);
 }
